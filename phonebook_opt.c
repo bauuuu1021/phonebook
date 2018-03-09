5c193dfd64575f46c980e6488d95121278ca41ed
bauuuu1021 <bboy010554@icloud.com>
2018-03-09 23:59:44 +0800

    Fuzzy search
    If the index word isn't in the list,
    find the closest word that has most letters same as the index word

--- phonebook_opt.c
+++ phonebook_opt.c
@@ -9,13 +9,36 @@ entry *findName(char lastName[], entry *pHead)
     entry *find;
     find=prefix[lastName[0]-97].pNext;
 
-    while (find != NULL) {
+    while (find != NULL) {  //the lastname can be found
         if (strcasecmp(lastName, find->lastName) == 0)
             return find;
         find = find->pNext;
     }
 
-    return NULL;
+    //can't find, fuzzy search
+    entry *bestFit, *current;
+    int i=0;
+    int sameLetterMax=0, sameLetter=0;
+    current=prefix[lastName[0]-97].pNext;
+
+    while (current!=NULL) {
+        while (lastName[i]!='\0' && current->lastName[i]!='\0') {   //check if ith letter is the same
+            if (lastName[i] == current->lastName[i])
+                sameLetter++;
+            i++;
+        }
+        if (sameLetter>sameLetterMax) { //if new bestfit appear, switch current and max
+            bestFit = current;
+            sameLetterMax=sameLetter;
+        }
+        sameLetter=0;
+        current=current->pNext;
+        i=0;
+    }
+
+    printf("Couldn't find '%s', did you mean '%s'??\n", lastName, bestFit);
+
+    return lastName;
 }
 
 entry *append(char lastName[], entry *e)
